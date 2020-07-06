070bef50f72978b69183abdc128af7e086361602
chug2k <chug2k@penguin>
2020-07-06 18:31:29 +0700

    slider2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import ExperiencesList from "./components/ExperiencesList";
 import AddExperience from "./components/AddExperience";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "rheostat/initialize";
+import "rheostat/css/rheostat.css";
 
 function App() {
   return (
--- src/components/ExperiencesList.js
+++ src/components/ExperiencesList.js
@@ -10,23 +10,83 @@ import {
   CardSubtitle,
   CardText,
 } from "reactstrap";
+import PaginationLink from "./PaginationLink";
+import Rheostat from "rheostat";
 
 const ExperiencesList = () => {
   const [experiences, setExperiences] = useState([]);
+  const [pageNum, setPageNum] = useState(1);
+  const [maxPageNum, setMaxPageNum] = useState(1);
+  const [minPrice, setMinPrice] = useState(1);
+  const [maxPrice, setMaxPrice] = useState(1000);
+  const [tempMinPrice, setTempMinPrice] = useState(1);
+  const [tempMaxPrice, setTempMaxPrice] = useState(1000);
+  const [isDragging, setIsDragging] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
-      const data = await fetch("http://localhost:3000/experiences");
-      const experiences = await data.json();
-      setExperiences(experiences);
+      const data = await fetch(
+        `http://localhost:3000/experiences?page=${pageNum}&minPrice=${minPrice}&maxPrice=${maxPrice}`
+      );
+      const resp = await data.json();
+      setExperiences(resp.data);
+      setMaxPageNum(parseInt(resp.maxPageNum));
     }
     fetchData();
-  }, []);
+  }, [pageNum, minPrice, maxPrice]);
+
+  const goNextPage = () => {
+    setPageNum(pageNum + 1);
+  };
+
+  const goPrevPage = () => {
+    setPageNum(pageNum - 1);
+  };
+
+  const handleChange = (e) => {
+    setMinPrice(e.values[0]);
+    setMaxPrice(e.values[1]);
+  };
+  const handleValuesUpdated = (e) => {
+    setTempMinPrice(e.values[0]);
+    setTempMaxPrice(e.values[1]);
+  };
 
   return (
     <Container>
       <h1> Experiences </h1>
-      <Row>
+      <div>
+        <Rheostat
+          min={1}
+          max={1000}
+          values={[minPrice, maxPrice]}
+          onChange={handleChange}
+          onValuesUpdated={handleValuesUpdated}
+          onSliderDragStart={() => setIsDragging(true)}
+          onSliderDragEnd={() => setIsDragging(false)}
+        />
+        <input
+          type="text"
+          value={minPrice}
+          onChange={(e) => setMinPrice(e.target.value)}
+        />
+        <p>
+          Min Price {tempMinPrice} - Max Price {tempMaxPrice}
+        </p>
+      </div>
+      <div className="d-flex justify-content-between">
+        <PaginationLink disabled={pageNum === 1} handleClick={goPrevPage}>
+          Prev Page
+        </PaginationLink>
+        <PaginationLink
+          disabled={pageNum === maxPageNum}
+          handleClick={goNextPage}
+        >
+          Next Page
+        </PaginationLink>
+      </div>
+
+      <Row style={{ opacity: isDragging ? "0.5" : "1" }}>
         {experiences.map((e) => (
           <Col sm={4}>
             <Experience {...e} />
