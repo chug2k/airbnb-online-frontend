e59cffffa28a75e31627be688fe287e1a1d21929
chug2k <chug2k@penguin>
2020-07-06 08:16:31 +0700

    monday

--- src/components/ExperiencesList.js
+++ src/components/ExperiencesList.js
@@ -13,19 +13,35 @@ import {
 
 const ExperiencesList = () => {
   const [experiences, setExperiences] = useState([]);
+  const [pageNum, setPageNum] = useState(1);
 
   useEffect(() => {
     async function fetchData() {
-      const data = await fetch("http://localhost:3000/experiences");
+      const data = await fetch(
+        `http://localhost:3000/experiences?page=${pageNum}`
+      );
       const experiences = await data.json();
       setExperiences(experiences);
     }
     fetchData();
-  }, []);
+  }, [pageNum]);
+
+  const prevPage = () => {
+    setPageNum(pageNum - 1);
+  };
+  const nextPage = () => {
+    setPageNum(pageNum + 1);
+  };
 
   return (
     <Container>
       <h1> Experiences </h1>
+      <div className="d-flex justify-content-between">
+        <PageLink onClick={prevPage} disabled={pageNum == 1}>
+          Prev Page
+        </PageLink>
+        <PageLink onClick={nextPage}>Next Page</PageLink>
+      </div>
       <Row>
         {experiences.map((e) => (
           <Col sm={4}>
@@ -34,6 +50,17 @@ const ExperiencesList = () => {
         ))}
       </Row>
     </Container>
+  );
+};
+
+const PageLink = ({ disabled, onClick, children }) => {
+  if (disabled) {
+    return <a class="btn disabled">{children}</a>;
+  }
+  return (
+    <a href="#" onClick={onClick}>
+      {children}
+    </a>
   );
 };
 
